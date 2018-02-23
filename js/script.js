9b9d884677dbc66c761301b45c3c021a77d2e31b
muturi254 <petermuturi74@gmail.com>
2018-02-23 12:19:21 +0300

    created logic in js for the ping pog game

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-    var input
+    var input = parseInt(prompt("Enter Number"));//get input from the user and change its type to strict interger
+    function pingPong(input){
+        // loop over from one to the range given in on the input
+        for (let i = 1; i <= input; i++) {
+            //check the divisiblity of the input if either quaifies in any condition
+            if ((i % 3 === 0) && (i % 5 === 0)) {
+                console.log("PingPong")
+            } else if (i % 3 === 0) {
+                console.log("ping");
+
+            } else if (i % 5 === 0) {
+                console.log("pong");
+            } else {
+                console.log(i)
+            }
+        }        
+    }
+    pingPong(input);
 });
