fd2a1e5f13a1c1bd445407ee5c4d852357b6c591
muturi254 <petermuturi74@gmail.com>
2018-02-26 10:31:45 +0300

    recreated js

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# PingPong [live game](https.muturi)
-## contributor
+# PingPong [live game](https://muturi254.github.io/pingPong/)
+
+## Contributor
 Peter muturi
+
+## Description
+
+
+## Technology used
+
+
+**jquery, css, bootstrap and html**
+
+## Set up instruction
+visit the [online](https://muturi254.github.io/pingPong/) site .To contribute fork or clone the project
+
+## lincense
+MIT &copy; 2018 [muturi](https://github.com/muturi254)
---
+++ js/script.js
@@ -0,0 +1,34 @@
+//business logic
+var pingArray = [];
+
+function pingPong(number) {
+    for (var index = 1; index <= number; index++) {
+        if (index % 15 === 0) {
+            pingArray.push("pingpong");
+        }
+        else if (index % 5 === 0) {
+            pingArray.push("pong");
+        }
+        else if (index % 3 === 0) {
+            pingArray.push("ping");
+        }
+        else {
+            pingArray.push(index);
+        }
+    }
+}
+
+
+//user interface logic
+
+$(document).ready(function () {
+    $("form#ping-pong").submit(function (event) {
+        event.preventDefault();        
+        var number = parseInt($("input#number").val());
+
+        pingPong(number);
+        pingArray.forEach(function (number) {
+            $("#output").append('<li>' + number + "</li>");
+        });
+    });
+});
